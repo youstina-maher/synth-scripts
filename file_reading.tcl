3b9897b28dc4fbea66525fd47771200e344dd0f4
youstina-maher <45272834+youstina-maher@users.noreply.github.com>
2018-11-23 19:48:58 +0200

    Update file_reading.tcl

--- file_reading.tcl
+++ file_reading.tcl
@@ -1,3 +1,6 @@
+# ########################################################################
+# This is an extra script that was just used for first-time testing
+# ########################################################################
 analyze -f verilog $DESIGN.v
 elaborate $DESIGN
 current_design $DESIGN
